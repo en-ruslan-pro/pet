e1c62c601a9ed5329a5d8423416594235a203df0
Ruslan MAC <myindexlike@gmail.com>
2026-09-03 09:30:05 +0200

    Add Virtual Pet TV landing page

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,9 @@
 <?php
 
-test('returns a successful response', function () {
-    $response = $this->get(route('home'));
-
-    $response->assertOk();
+test('shows the virtual pet landing page', function () {
+    $this->get(route('home'))
+        ->assertOk()
+        ->assertSee('Ваш питомец живёт на телевизоре.')
+        ->assertSee('Создать питомца')
+        ->assertSee(route('room.create'), false);
 });
